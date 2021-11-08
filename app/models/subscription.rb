4fb002ee1968378d37afb99d365df99b155e091c
Taylor Varoglu <tava8398@colorado.edu>
2021-11-08 13:24:35 -0700

    Add relationship and validation tests for Subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,15 @@
 class Subscription < ApplicationRecord
   belongs_to :customer
+
+  validates :customer_id, presence: true, numericality: true
+  validates :tea_id, presence: true
+  validates :description, presence: true
+  validates :temperature, presence: true, numericality: true
+  validates :brew_time, presence: true, numericality: true
+  validates :price, presence: true, numericality: true
+  validates :frequency, presence: true
+  validates :status, presence: true
+
+  enum frequency: { 'Weekly' => 0, 'Monthly' => 1, 'Annually' => 2 }
+  enum status: { 'Active' => 0, 'Cancelled' => 1 }
 end
