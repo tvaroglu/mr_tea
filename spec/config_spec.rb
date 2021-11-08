d4bdaa0d57744889f7c51d370577153e4220f0ac
Taylor Varoglu <tava8398@colorado.edu>
2021-11-08 12:15:46 -0700

    Remove test dir

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'project config test' do
   # TODO: remove file once request specs in place
   let(:hello) { 'world' }
+
   it 'can run a test test' do
     expect(hello).to eq 'world'
   end
