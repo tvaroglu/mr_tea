eeaf9c1aa2c4ffecdd54ec1992b01eead3e80b49
Taylor Varoglu <tava8398@colorado.edu>
2021-11-08 19:57:49 -0700

    Complete happy path testing for Subscriptions::Create

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :teas, only: [:index]
+      resources :customers, only: [] do
+        resources :subscriptions, only: [:index, :create, :update]
+      end
     end
   end
 end
---
+++ spec/requests/api/v1/subscriptions/create_request_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+# See spec/support/shared_examples/ for shared examples for tests where
+# `include_examples` is used. See Shared Examples in the RSpec docs for more
+# info:
+#   https://relishapp.com/rspec/rspec-core/docs/example-groups/shared-examples
+#
+# See spec/support/request_spec_helper.rb for #json and #json_data helpers.
+describe 'Subscriptions::Create', type: :request do
+  describe 'POST /api/v1/customers/:id/subscriptions' do
+    context 'when an existing customer wants to create a new subscription with valid attributes' do
+      let(:customer) { create(:customer) }
+
+      let(:tea) do
+        Tea.new({
+          _id: '5fa3fd48d5ba620017ec1c09',
+          name: 'green',
+          description: 'Rich in antioxidants and reduces inflammation.',
+          brew_time: 2,
+          temperature: 80
+        })
+      end
+
+      let(:valid_attributes) do
+        {
+          title: tea.name,
+          description: tea.description,
+          temperature: tea.temperature,
+          brew_time: tea.brew_time,
+          tea_id: tea.id,
+          price: Faker::Number.decimal(l_digits: 2),
+          frequency: %w[Weekly Monthly Annually].sample,
+          status: 'Active',
+          customer_id: customer.id
+        }
+      end
+
+      before { post "/api/v1/customers/#{customer.id}/subscriptions", params: valid_attributes }
+
+      it 'creates a new subscription for the customer', :aggregate_failures do
+        expect(json).not_to be_empty
+        expect(json_data.size).to eq 3
+
+        expect(json_data[:attributes].size).to eq 9
+        expect(json_data[:attributes][:customer_id]).to eq valid_attributes[:customer_id]
+        expect(json_data[:attributes][:tea_id]).to eq valid_attributes[:tea_id]
+        expect(json_data[:attributes][:title]).to eq valid_attributes[:title]
+        expect(json_data[:attributes][:description]).to eq valid_attributes[:description]
+        expect(json_data[:attributes][:temperature]).to eq valid_attributes[:temperature]
+        expect(json_data[:attributes][:brew_time]).to eq valid_attributes[:brew_time]
+        expect(json_data[:attributes][:price]).to eq valid_attributes[:price]
+        expect(json_data[:attributes][:frequency]).to eq valid_attributes[:frequency]
+        expect(json_data[:attributes][:status]).to eq valid_attributes[:status]
+      end
+
+      include_examples 'status code 201'
+    end
+  end
+end
