25052665f15c98124e722676e431598cecb39b87
Taylor Varoglu <tava8398@colorado.edu>
2021-11-08 17:26:10 -0700

    Add Response module to concerns

--- app/controllers/api/v1/teas_controller.rb
+++ app/controllers/api/v1/teas_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::TeasController < ApplicationController
   def index
     teas = TeaFacade.format_all_teas
-    render json: TeaSerializer.new(teas).serializable_hash, status: :ok
+    json_response(TeaSerializer.new(teas).serializable_hash)
   end
 end
